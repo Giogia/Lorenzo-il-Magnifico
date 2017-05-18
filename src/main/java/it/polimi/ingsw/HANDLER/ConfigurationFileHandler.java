f983f51048563a87b9e0c1e29736eb1ab79d8953
Michele <michi.popup@hotmail.it>
2017-05-19 00:12:24 +0200

    prove su json e gson

--- src/main/java/it/polimi/ingsw/HANDLER/ConfigurationFileHandler.java
+++ src/main/java/it/polimi/ingsw/HANDLER/ConfigurationFileHandler.java
@@ -1,11 +1,31 @@
 package it.polimi.ingsw.HANDLER;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+import it.polimi.ingsw.CARD.Territory;
 
 public class ConfigurationFileHandler {
 	public static void main(String[] args){
 		try {
+			Territory territorio1 = new Territory(1, 7, new ArrayList<>(), new ArrayList<>());
+			Territory territorio2 = new Territory(4, 9, new ArrayList<>(), new ArrayList<>());
+			ArrayList<Territory> prova= new ArrayList<>();
+			prova.add(territorio1);
+			prova.add(territorio2);
 			
+			Gson gson= new Gson();
 			
+			String inJson = gson.toJson(prova);
+			System.out.println(inJson);
+			
+			Type list= new TypeToken<ArrayList<Territory>>(){}.getType();
+			ArrayList<Territory> territori = gson.fromJson(inJson, list);
+			System.out.println(territori.get(0).period);
+			System.out.println(territori.get(1).period);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
