fb8456a20d93a09cf5995c88f96f62a0caaaf13b
Michele <michi.popup@hotmail.it>
2017-05-18 19:08:31 +0200

    modifica SOSTANZIALE. spostato l'array dei figli in LeaderCard

--- src/main/java/it/polimi/ingsw/CARD/LeaderCard.java
+++ src/main/java/it/polimi/ingsw/CARD/LeaderCard.java
@@ -2,15 +2,18 @@ package it.polimi.ingsw.CARD;
 
 import java.util.ArrayList;
 
+import it.polimi.ingsw.BONUS.Bonus;
 import it.polimi.ingsw.RESOURCE.Resource;
 
 public abstract class LeaderCard {
 	private ArrayList<Resource> resourceActivationCondition;
 	private ArrayList<DevelopmentCard> cardActivationCondition;
+	public final ArrayList<Bonus> bonus;
 	
-	public LeaderCard(ArrayList<Resource> resourceActivationCondition, ArrayList<DevelopmentCard> cardActivationCondition) {
+	public LeaderCard(ArrayList<Resource> resourceActivationCondition, ArrayList<DevelopmentCard> cardActivationCondition, ArrayList<Bonus> bonus) {
 		this.resourceActivationCondition=resourceActivationCondition;
 		this.cardActivationCondition=cardActivationCondition;
+		this.bonus=bonus;
 	}
 	
 	public ArrayList<DevelopmentCard> getCardActivationCondition() {
--- src/main/java/it/polimi/ingsw/CARD/PermanentLeaderCard.java
+++ src/main/java/it/polimi/ingsw/CARD/PermanentLeaderCard.java
@@ -6,11 +6,9 @@ import it.polimi.ingsw.BONUS.Bonus;
 import it.polimi.ingsw.RESOURCE.Resource;
 
 public class PermanentLeaderCard extends LeaderCard{
-	public final ArrayList<Bonus> permanentBonus;
 	
 	public PermanentLeaderCard(ArrayList<Bonus> permanentBonus, ArrayList<Resource> resourceActivationCondition, ArrayList<DevelopmentCard> cardActivationCondition) {
-		super(resourceActivationCondition, cardActivationCondition);
-		this.permanentBonus=permanentBonus;
+		super(resourceActivationCondition, cardActivationCondition, permanentBonus);
 	}
 	
 	public void activatePermanentBonus(){
