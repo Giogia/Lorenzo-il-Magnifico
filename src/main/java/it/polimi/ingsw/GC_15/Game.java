38b53d114d37946f516d770d83b288ce5ec3e13c
Michele <michi.popup@hotmail.it>
2017-05-21 12:01:14 +0200

    eliminato attributi inutili

--- src/main/java/it/polimi/ingsw/GC_15/Game.java
+++ src/main/java/it/polimi/ingsw/GC_15/Game.java
@@ -1,14 +1,5 @@
 package it.polimi.ingsw.GC_15;
 
-import java.util.ArrayList;
-
-import it.polimi.ingsw.CARD.Building;
-import it.polimi.ingsw.CARD.Territory;
-import it.polimi.ingsw.CARD.Venture;
-
 public class Game {
-	public final ArrayList<Territory> Territories;
-	public final ArrayList<Building> Buildings;
-	public final ArrayList<Venture> Ventures;
-	public final ArrayList<Character> Characters;
+	
 }
