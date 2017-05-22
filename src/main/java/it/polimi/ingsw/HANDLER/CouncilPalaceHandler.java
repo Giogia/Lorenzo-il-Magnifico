1482a8da219fe9d4576a625e0d5351901cbb6e9a
giova <giova@DESKTOP-QLEF867>
2017-05-22 18:58:58 +0200

    MarketHandler e CouncilPalaceHandler

--- src/main/java/it/polimi/ingsw/HANDLER/CouncilPalaceHandler.java
+++ src/main/java/it/polimi/ingsw/HANDLER/CouncilPalaceHandler.java
@@ -2,23 +2,16 @@ package it.polimi.ingsw.HANDLER;
 
 import it.polimi.ingsw.BOARD.Position;
 import it.polimi.ingsw.CONTROLLER.FamilyMemberValueController;
-import it.polimi.ingsw.CONTROLLER.PositionAlreadyOccupiedController;
 import it.polimi.ingsw.GC_15.FamilyMember;
 
-public class MarketHandler {
+public class CouncilPalaceHandler {
 	
 	public static boolean handle(FamilyMember familyMember, Position position){
-		if (!PositionAlreadyOccupiedController.check(position)){
-			if (!OccupiedYetBonusController.check(familyMember)){
-				return false;
-			}
-		}
 		if (FamilyMemberValueController.check(familyMember, position)){
 			familyMember.getPlayer().setFamilyMemberPosition(familyMember, position);
 			return true;
 		}
 		return false;
-			
 	}
 
 }
--- src/main/java/it/polimi/ingsw/HANDLER/TowerHandler.java
+++ src/main/java/it/polimi/ingsw/HANDLER/TowerHandler.java
@@ -9,7 +9,16 @@ import it.polimi.ingsw.CONTROLLER.*;
 
 public class TowerHandler {
 
-	public static void check(FamilyMember familyMember, Tower zone, TowerFloor towerFloor) {
+	public static void handle(FamilyMember familyMember, Tower zone, TowerFloor towerFloor) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		if( PositionAlreadyOccupiedController(towerFloor) &&
 			ZoneOccupiedBySameColorController(zone, towerFloor) &&
 			EnoughSpaceInPersonalBoard(familyMember.getPlayer(), towerFloor.getDevelopmentCard()) &&
