00589099b71e41e2c07338eb208971c4cf6ba3ce
Michele <michi.popup@hotmail.it>
2017-05-27 23:02:16 +0200

    various fix

--- src/main/java/it/polimi/ingsw/HANDLER/GAME/ConfigurationFileHandler.java
+++ src/main/java/it/polimi/ingsw/HANDLER/GAME/ConfigurationFileHandler.java
@@ -73,7 +73,7 @@ public class ConfigurationFileHandler {
 		int[] victoryPointsForMilitaryCard = new int[6];
 		//military points requirement for territory card
 		int[] militaryRequirement = new int[6];	
-		/*for(int i= 0; i < 32; i++){
+		for(int i= 0; i < 32; i++){
 			System.out.println("creare la prossima carta territorio: ");
 			territories.add(Create.createTerritory());
 		}
@@ -87,7 +87,7 @@ public class ConfigurationFileHandler {
 			System.out.println("creare la prossima carta azione: ");
 			ventures.add(Create.createVenture());
 		}
-		DataFromFile.setVentures(ventures);*/
+		DataFromFile.setVentures(ventures);
 		for(int i= 0; i < 32; i++){
 			System.out.println("creare la prossima carta personaggio: ");
 			characters.add(Create.createCharacter());
@@ -98,6 +98,7 @@ public class ConfigurationFileHandler {
 			excommunicationTiles.add(Create.createExcommunicationTile());
 		}
 		DataFromFile.setExcommunicationTiles(excommunicationTiles);
+		
 		for(int i=0; i < 5; i++){
 			System.out.println("inserire le personal bonus tile (NB: ti verrà chiesto 5 volte)");
 			personalBonusTiles.add(Create.createPersonalBonusTile());
