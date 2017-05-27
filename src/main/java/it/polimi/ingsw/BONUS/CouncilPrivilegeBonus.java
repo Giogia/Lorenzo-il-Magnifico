ff2d06dbc3cfe377901c65ffede0c9f0be6b2041
Nontrovounnick <giovanni.probo@mail.polimi.it>
2017-05-27 19:01:55 +0200

    Fixed CouncilPrivilegeBonus

--- src/main/java/it/polimi/ingsw/BONUS/CouncilPrivilegeBonus.java
+++ src/main/java/it/polimi/ingsw/BONUS/CouncilPrivilegeBonus.java
@@ -3,26 +3,21 @@ package it.polimi.ingsw.BONUS;
 import java.util.ArrayList;
 
 import it.polimi.ingsw.GC_15.Player;
+import it.polimi.ingsw.HANDLER.CouncilPrivilegeChoiceHandler;
 
 public class CouncilPrivilegeBonus extends ImmediateBonus{
 	private int differentCouncilPrivilege;
-	private ArrayList<ResourceBonus> resourceBonus;
 	
-	public CouncilPrivilegeBonus(int number, ArrayList<ResourceBonus> resource){
+	public CouncilPrivilegeBonus(int number){
 		super("councilPrivilegeBonus");
 		differentCouncilPrivilege = number;
-		resourceBonus.equals(resource);
 	}
 	
-	/*public void getImmediateBonus(Player player){
-		while (differentCouncilPrivilege > 0){
-			player.choosePrivilegeCouncil(resourceBonus);
-			TODO DA SISTEMARE!!!!!
-			Si può fare in due modi:
-			- Si chiama un'altra funzione, che chiama choosePrivilegeCouncil e ritorna a questa funzione il ResourceBonus che ha scelto
-			- Fa tutto il player e questa funzione da al Player il numero di privilegi da scegliere e la lista
-			
-		}
-	}*/
+	
+	@Override
+	public void getImmediateBonus(Player player) {
+		CouncilPrivilegeChoiceHandler.handle(player, differentCouncilPrivilege);
+	}
+	
 
 }
