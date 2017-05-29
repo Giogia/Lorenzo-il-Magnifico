ecbf036420f2b5d0f6e66544d8dac6c795fa6860
Nontrovounnick <giovanni.probo@mail.polimi.it>
2017-05-29 16:28:27 +0200

    FakeFamilyMemberHandler

--- src/main/java/it/polimi/ingsw/BONUS/ActionBonus.java
+++ src/main/java/it/polimi/ingsw/BONUS/ActionBonus.java
@@ -1,11 +1,13 @@
 package it.polimi.ingsw.BONUS;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
 import it.polimi.ingsw.BOARD.ActionZone;
 import it.polimi.ingsw.GC_15.Player;
 import it.polimi.ingsw.HANDLER.FakeFamilyMemberHandler;
+import it.polimi.ingsw.RESOURCE.Resource;
 
 public class ActionBonus extends ImmediateBonus {
 	private HashMap<ActionZone, Integer> action;
