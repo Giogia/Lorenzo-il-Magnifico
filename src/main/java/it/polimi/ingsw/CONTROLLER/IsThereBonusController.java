14223b989e73fce509e47a632eff41d5edb81cb7
Michele <michele.papale@mail.polimi.it>
2017-06-03 14:16:02 +0200

    fixed this controller

--- src/main/java/it/polimi/ingsw/CONTROLLER/IsThereBonusController.java
+++ src/main/java/it/polimi/ingsw/CONTROLLER/IsThereBonusController.java
@@ -11,8 +11,8 @@ public class IsThereBonusController {
 	// Prende TowerFloor, perchè nelle altre posizioni non ha senso controllare. Serviva solo per usare il bonus per prendere la carta
 	public static boolean check(TowerFloor towerFloor) throws Exception{
 		ArrayList<ImmediateBonus> allBonus = towerFloor.getBoardBonus();
-		if(allBonus.isEmpty()){
-			throw new Exception("there is no bonus in this position");
+		if(allBonus == null){
+			throw new Exception("There is no bonus in this position\n");
 		}
 		return true;
 	}
