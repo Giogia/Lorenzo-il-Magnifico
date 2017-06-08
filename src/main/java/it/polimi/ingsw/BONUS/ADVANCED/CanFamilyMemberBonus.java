e7454d111f06fc31fe253a503737a4e3e1861fc2
Nontrovounnick <giovanni.probo@mail.polimi.it>
2017-06-08 20:30:08 +0200

    OccupiedYetBonusController

--- src/main/java/it/polimi/ingsw/BONUS/ADVANCED/CanFamilyMemberBonus.java
+++ src/main/java/it/polimi/ingsw/BONUS/ADVANCED/CanFamilyMemberBonus.java
@@ -1,8 +1,16 @@
 package it.polimi.ingsw.BONUS.ADVANCED;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import it.polimi.ingsw.BOARD.CouncilPalace;
+import it.polimi.ingsw.BOARD.HarvestArea;
+import it.polimi.ingsw.BOARD.Market;
+import it.polimi.ingsw.BOARD.ProductionArea;
+import it.polimi.ingsw.BOARD.Tower;
 import it.polimi.ingsw.BOARD.Zone;
+import it.polimi.ingsw.CARD.DevelopmentCardType;
+import it.polimi.ingsw.GC_15.Player;
 
 public class CanFamilyMemberBonus extends PermanentBonus{
 	//di default false: se è occupato il familyMember non può andarci
@@ -24,5 +32,101 @@ public class CanFamilyMemberBonus extends PermanentBonus{
 	
 	public boolean getOccupiedYet() {
 		return occupiedYet;
+	}
+	
+	@Override
+	public void getPermanentBonus(Player player) {
+		ArrayList<PermanentBonus> playerBonus = player.getPersonalBoard().getPermanentBonus();
+		if (playerBonus != null){
+			for (PermanentBonus permanentBonus : playerBonus) {
+				if (permanentBonus instanceof CanFamilyMemberBonus){
+					((CanFamilyMemberBonus) permanentBonus).addBonus(this);
+					return;
+				}
+			}
+		}
+		super.getPermanentBonus(player);
+	}
+	
+	public void addBonus(CanFamilyMemberBonus canFamilyMemberBonus){
+		this.occupiedYet = this.occupiedYet || canFamilyMemberBonus.getOccupiedYet();
+		for (Zone zone : canFamilyMemberBonus.getCanGoTo().keySet()) {
+			Zone bonusZone = null;
+			if (zone instanceof Tower){
+				bonusZone = findTower(((Tower) zone).getDevelopmentCardType());
+			}
+			else if (zone instanceof HarvestArea){
+				bonusZone = findHarvest();
+			}
+			else if (zone instanceof ProductionArea){
+				bonusZone = findProduction();
+			}
+			else if (zone instanceof Market){
+				bonusZone = findMarket();
+			}
+			else {
+				bonusZone = findCouncilPalace();
+			}
+			if (bonusZone == null){
+				canGoTo.put(zone, canFamilyMemberBonus.getCanGoTo().get(zone));
+			}
+			else{
+				Boolean oldValue = canGoTo.get(bonusZone);
+				Boolean newValue = oldValue && canFamilyMemberBonus.getCanGoTo().get(zone);
+				canGoTo.replace(bonusZone, oldValue, newValue);
+			}
+		}
+	}
+
+
+	private Zone findCouncilPalace() {
+		for (Zone zone : canGoTo.keySet()) {
+			if (zone instanceof CouncilPalace){
+				return zone;
+			}
+		}
+		return null;
+	}
+
+
+	private Zone findMarket() {
+		for (Zone zone : canGoTo.keySet()) {
+			if (zone instanceof Market){
+				return zone;
+			}
+		}
+		return null;
+	}
+
+
+	private Zone findProduction() {
+		for (Zone zone : canGoTo.keySet()) {
+			if (zone instanceof ProductionArea){
+				return zone;
+			}
+		}
+		return null;
+	}
+
+
+	private Zone findHarvest() {
+		for (Zone zone : canGoTo.keySet()) {
+			if (zone instanceof HarvestArea){
+				return zone;
+			}
+		}
+		return null;
+	}
+
+
+	private Zone findTower(DevelopmentCardType developmentCardType) {
+		for (Zone zone : canGoTo.keySet()) {
+			if (zone instanceof Tower){
+				if (((Tower) zone).getDevelopmentCardType().equals(developmentCardType)){
+					return zone;
+				}
+			}
+		}
+		return null;
 	}
 }
--- src/main/java/it/polimi/ingsw/CONTROLLER/OccupiedYetBonusController.java
+++ src/main/java/it/polimi/ingsw/CONTROLLER/OccupiedYetBonusController.java
@@ -1,9 +1,23 @@
 package it.polimi.ingsw.CONTROLLER;
 
+import java.util.ArrayList;
+
+import it.polimi.ingsw.BONUS.ADVANCED.CanFamilyMemberBonus;
+import it.polimi.ingsw.BONUS.ADVANCED.PermanentBonus;
 import it.polimi.ingsw.GC_15.FamilyMember;
 
 public class OccupiedYetBonusController {
+	
 	public static boolean check(FamilyMember familyMember){
-		return false; //TODO PERMANENT cosi il resto funziona ma e' da implementare
+		ArrayList<PermanentBonus> playerBonus = familyMember.getPlayer().getPersonalBoard().getPermanentBonus();
+		if (playerBonus != null){
+			for (PermanentBonus permanentBonus : playerBonus) {
+				if (permanentBonus instanceof CanFamilyMemberBonus){
+					return ((CanFamilyMemberBonus) permanentBonus).getOccupiedYet();
+				}
+			}
+		}
+		return false; 
 	}
+	
 }
