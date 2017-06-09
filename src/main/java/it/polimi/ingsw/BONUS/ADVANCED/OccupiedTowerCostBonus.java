c2a5fc1c95e969274783c8fc6235828903c9ee7e
Nontrovounnick <giovanni.probo@mail.polimi.it>
2017-06-09 11:44:28 +0200

    Created getPermanentBonus in OccupiedTowerCostBonus

--- src/main/java/it/polimi/ingsw/BONUS/ADVANCED/OccupiedTowerCostBonus.java
+++ src/main/java/it/polimi/ingsw/BONUS/ADVANCED/OccupiedTowerCostBonus.java
@@ -1,5 +1,9 @@
 package it.polimi.ingsw.BONUS.ADVANCED;
 
+import java.util.ArrayList;
+
+import it.polimi.ingsw.GC_15.Player;
+
 public class OccupiedTowerCostBonus extends PermanentBonus {
 	private int addOccupiedCost;
 	private int multOccupiedCost;
@@ -16,6 +20,31 @@ public class OccupiedTowerCostBonus extends PermanentBonus {
 	
 	public int getMultOccupiedCost() {
 		return multOccupiedCost;
+	}
+	
+	public void setAddOccupiedCost(int addOccupiedCost) {
+		this.addOccupiedCost = addOccupiedCost;
+	}
+	
+	public void setMultOccupiedCost(int multOccupiedCost) {
+		this.multOccupiedCost = multOccupiedCost;
+	}
+	
+	@Override
+	public void getPermanentBonus(Player player) {
+		ArrayList<PermanentBonus> playerBonus = player.getPersonalBoard().getPermanentBonus();
+		for (PermanentBonus permanentBonus : playerBonus) {
+			if (permanentBonus instanceof OccupiedTowerCostBonus){
+				int addOccupiedCost = ((OccupiedTowerCostBonus) permanentBonus).getAddOccupiedCost();
+				int multOccupiedCost = ((OccupiedTowerCostBonus) permanentBonus).getMultOccupiedCost();
+				addOccupiedCost = addOccupiedCost + this.addOccupiedCost;
+				multOccupiedCost = multOccupiedCost * this.multOccupiedCost;
+				((OccupiedTowerCostBonus) permanentBonus).setAddOccupiedCost(addOccupiedCost);
+				((OccupiedTowerCostBonus) permanentBonus).setMultOccupiedCost(multOccupiedCost);
+				return;
+			}
+		}
+		super.getPermanentBonus(player);
 	}
 	
 }
