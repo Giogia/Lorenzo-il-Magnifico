b88b24eb9fd02ba2ebd6f63c60e629e8118591d2
Nontrovounnick <giovanni.probo@mail.polimi.it>
2017-06-12 12:08:41 +0200

    Fix excommunicationTile

--- src/main/java/it/polimi/ingsw/GC_15/ExcommunicationTile.java
+++ src/main/java/it/polimi/ingsw/GC_15/ExcommunicationTile.java
@@ -1,25 +1,34 @@
 package it.polimi.ingsw.GC_15;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import it.polimi.ingsw.BONUS.Bonus;
 import it.polimi.ingsw.BONUS.ADVANCED.PermanentBonus;
 
 public class ExcommunicationTile implements Serializable{
 	public final int period;
-	public final PermanentBonus malus;
+	public final ArrayList<PermanentBonus> malus;
 	
-	public ExcommunicationTile(int period, PermanentBonus malus) {
+	public ExcommunicationTile(int period, ArrayList<PermanentBonus> malus) {
 		this.period=period;
 		this.malus= malus;
 	}
 	
 	public void giveMalus(Player player){
-		malus.getPermanentBonus(player);
+		for (PermanentBonus permanentBonus : malus) {
+			permanentBonus.getPermanentBonus(player);
+		}
 	}
 
 	public String getDescription() {
-		String description = "Period: " + period + "\nMalus: " + malus.getDescription() + "\n";
+		String description = "Period: " + period + "\n";
+		if (malus != null){
+			description = description + "Malus: \n";
+			for (PermanentBonus permanentBonus : malus) {
+				description = description + permanentBonus.getDescription() + "\n";
+			}
+		}
 		return description;
 	}
 }
